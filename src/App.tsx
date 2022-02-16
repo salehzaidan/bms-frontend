5c84ac889961a911d43b87c6cef111c16804dfd5
Saleh Zaidan <salehzaidan64@gmail.com>
2022-02-16 22:16:37 +0700

    Add container for main content

--- src/App.tsx
+++ src/App.tsx
@@ -24,46 +24,46 @@ function App() {
         <header className="bg-teal-100 p-4">
           <h1 className="text-center text-2xl">Battery Management System</h1>
         </header>
-        <main className="mx-auto mt-8 grid max-w-screen-sm grid-cols-1 gap-6 px-4 lg:max-w-screen-lg lg:grid-cols-2">
-          <Card title="Battery Status">
-            <BatteryGauge value={data.general[0].avg_soc} />
-            <DataItem
-              className="row-start-2"
-              label="Total Voltage"
-              variable={Variable.VOLTAGE}
-              value={data.general[0].total_voltage}
-              variant="horizontal"
-            />
-          </Card>
-
-          <div className="grid grid-rows-3 gap-6 sm:grid-cols-3 sm:grid-rows-none">
-            <DataInfo
-              variable={Variable.VOLTAGE}
-              min={data.general[0].min_voltage}
-              average={data.general[0].avg_voltage}
-              max={data.general[0].max_voltage}
-            />
-            <DataInfo
-              variable={Variable.TEMPERATURE}
-              min={data.general[0].min_temperature}
-              average={data.general[0].avg_temperature}
-              max={data.general[0].max_temperature}
-            />
-            <DataInfo
-              variable={Variable.SOC}
-              min={data.general[0].min_soc}
-              average={data.general[0].avg_soc}
-              max={data.general[0].max_soc}
-            />
-          </div>
-
-          <Card title="Cell Status" className="lg:col-span-2">
-            <div className="grid grid-cols-2 justify-items-center gap-8 sm:grid-cols-4 lg:grid-cols-5">
-              {data.value.map(item => (
-                <BatteryCell key={item.cell_id} value={item} />
-              ))}
+        <main className="py-8 px-4 lg:p-16">
+          <div className="mx-auto grid max-w-screen-sm grid-cols-1 gap-6 lg:max-w-screen-lg lg:grid-cols-2">
+            <Card title="Battery Status">
+              <BatteryGauge value={data.general[0].avg_soc} />
+              <DataItem
+                className="row-start-2"
+                label="Total Voltage"
+                variable={Variable.VOLTAGE}
+                value={data.general[0].total_voltage}
+                variant="horizontal"
+              />
+            </Card>
+            <div className="grid grid-rows-3 gap-6 sm:grid-cols-3 sm:grid-rows-none">
+              <DataInfo
+                variable={Variable.VOLTAGE}
+                min={data.general[0].min_voltage}
+                average={data.general[0].avg_voltage}
+                max={data.general[0].max_voltage}
+              />
+              <DataInfo
+                variable={Variable.TEMPERATURE}
+                min={data.general[0].min_temperature}
+                average={data.general[0].avg_temperature}
+                max={data.general[0].max_temperature}
+              />
+              <DataInfo
+                variable={Variable.SOC}
+                min={data.general[0].min_soc}
+                average={data.general[0].avg_soc}
+                max={data.general[0].max_soc}
+              />
             </div>
-          </Card>
+            <Card title="Cell Status" className="lg:col-span-2">
+              <div className="grid grid-cols-2 justify-items-center gap-8 sm:grid-cols-4 lg:grid-cols-5">
+                {data.value.map(item => (
+                  <BatteryCell key={item.cell_id} value={item} />
+                ))}
+              </div>
+            </Card>
+          </div>
         </main>
       </div>
     </AppCtx.Provider>
