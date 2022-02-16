d87cdc29a3c2208d8e8f9aa82faaee1480e5c32e
Saleh Zaidan <salehzaidan64@gmail.com>
2022-02-16 22:21:33 +0700

    Update card border

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,12 @@ interface CardProps extends Props {
 
 function Card({ title, children, className = '' }: CardProps) {
   return (
-    <div className={classNames(className, 'flex flex-col rounded shadow')}>
+    <div
+      className={classNames(
+        className,
+        'flex flex-col overflow-hidden rounded-md border border-gray-200 shadow'
+      )}
+    >
       <h2 className="bg-teal-100 px-4 py-2 font-medium">{title}</h2>
       <div className="flex flex-1 flex-col items-center justify-center gap-4 bg-white p-4">
         {children}
